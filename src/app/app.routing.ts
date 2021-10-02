bc315087c1243624ee8e0c986e5b5e2b8ed7123d
SAJID <sajidrbhat@gmail.com>
2021-10-02 19:58:50 +0530

    2nd psuh

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,34 +1,51 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { PostArticleComponent } from "./article/post-article/post-article.component";
+import { IssueListComponent } from "./issue-list/issue-list.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
 
-const routes: Routes =[
+const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '',
+    path: "welcome",
+    component: WelcomeComponent,
+    children: [
+      { path: "issue-list", component: IssueListComponent },
+      { path: "post-article", component: PostArticleComponent },
+    ],
+  },
+
+  {
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full",
+  },
+  {
+    path: "",
     component: AdminLayoutComponent,
-    children: [{
-      path: '',
-      loadChildren: () => import('./layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
-    }]
-  }
+    children: [
+      {
+        path: "",
+        loadChildren: () =>
+          import("./layouts/admin-layout/admin-layout.module").then(
+            (m) => m.AdminLayoutModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
-    })
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
   ],
-  exports: [
-  ],
+  exports: [],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
